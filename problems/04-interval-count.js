7f1c93c7a0cc6a6135673fd556213618e4d03429
Brendan <blau4000@gmail.com>
2022-03-31 13:49:35 -0400

    04

--- problems/04-interval-count.js
+++ problems/04-interval-count.js
@@ -8,18 +8,22 @@ Hint: utilize a 'closure' to your advantage
 
 In addition to Mocha, we recommend that you test your code manually using 
 node with the examples below.
-
-Example
-
-intervalCount(function() {
-    console.log('hi');
-}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 ***********************************************************************/
 
 function intervalCount(cb, delay, amount) {
-  // Your code here
+  let count = 0;
+  const int = setInterval(() => {
+    cb();
+    count++;
+    if (count === amount) clearInterval(int)
+  }, delay)
 }
 
+
+
+intervalCount(function () {
+  console.log('hi');
+}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = intervalCount;
